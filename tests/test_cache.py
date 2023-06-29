d34114855ee4920f2ef5367121dc460c7bde5c10
Antoine <caillonantoine@gmail.com>
2023-06-29 16:52:07 +0200

    update tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -61,46 +61,6 @@ def test_decoder_only():
 
 
 @load_configuration(
-    path="encoder_decoder.gin",
-    overrides=["MODEL_DIM = 16", "HEAD_DIM = 2", "NUM_LAYERS = 2"],
-)
-def test_encoder_decoder():
-    cc.use_cached_conv(False)
-    regular = Prior().eval()
-
-    cc.use_cached_conv(True)
-    streaming = Prior().eval()
-
-    for p1, p2 in zip(regular.parameters(), streaming.parameters()):
-        p2.data.copy_(p1.data)
-
-    input_params = gin.get_bindings("attention.MultivariateEmbedding")
-    inputs = torch.randint(
-        0,
-        input_params["num_tokens"],
-        (1, 128, input_params["num_quantizers"]),
-    )
-
-    semantic_params = gin.get_bindings("encoder/attention.Embedding")
-    semantic = torch.randint(
-        0,
-        semantic_params["num_embeddings"],
-        (1, 32),
-    )
-
-    outputs_regular = regular(inputs, semantic, targets=inputs)
-
-    num_chunks = 4
-    outputs_streaming = []
-    for inputs, semantic in zip(inputs.chunk(num_chunks, 1),
-                                semantic.chunk(num_chunks, 1)):
-        outputs_streaming.append(streaming(inputs, semantic, targets=inputs))
-    outputs_streaming = torch.cat(outputs_streaming, 1)
-
-    assert torch.allclose(outputs_regular, outputs_streaming, 1e-4, 1e-4)
-
-
-@load_configuration(
     path="decoder_only.gin",
     overrides=["MODEL_DIM = 16", "HEAD_DIM = 2", "NUM_LAYERS = 2"],
 )
@@ -138,68 +98,6 @@ def test_decoder_only_nn_tilde():
     # AUTOREGRESSIVE SAMPLE USING NN_TILDE
     streaming.previous_step[:1].copy_(inputs)
     out = streaming(torch.zeros(1, 16, 4))[:, :-1]
-    streaming_samples = torch.cat([inputs, out.permute(0, 2, 1)], 1).long()
-
-    assert torch.allclose(regular_samples, streaming_samples)
-
-
-@load_configuration(
-    path="encoder_decoder.gin",
-    overrides=["MODEL_DIM = 8", "HEAD_DIM = 2", "NUM_LAYERS = 1"],
-)
-def test_encoder_decoder_nn_tilde():
-    cc.use_cached_conv(False)
-    regular = Prior().eval()
-
-    cc.use_cached_conv(True)
-    streaming = ScriptedPrior().eval()
-
-    for p1, p2 in zip(regular.parameters(), streaming.parameters()):
-        p2.data.copy_(p1.data)
-
-    input_params = gin.get_bindings("attention.MultivariateEmbedding")
-    inputs = torch.randint(
-        0,
-        input_params["num_tokens"],
-        (1, 1, input_params["num_quantizers"]),
-    )
-
-    semantic_params = gin.get_bindings("encoder/attention.Embedding")
-    semantic = torch.randint(
-        0,
-        semantic_params["num_embeddings"],
-        (1, 16),
-    )
-
-    # AUTOREGRESSIVE SAMPLE USING REGULAR MODEL
-    regular_samples = [inputs]
-
-    regular_encoder_out = regular.encoder(semantic)
-
-    for i in range(64):
-        encoder_out_in = regular_encoder_out[:, :i // 4 + 1]
-        codes_in = torch.cat(regular_samples, 1)
-        sample, _ = regular.sample(
-            codes_in,
-            encoder_out_in,
-            temperature=0,
-        )
-        regular_samples.append(sample[:, -1:])
-
-    regular_samples = torch.cat(regular_samples, 1)
-
-    # AUTOREGRESSIVE SAMPLE USING NN_TILDE
-    streaming.set_listen(False)
-    streaming.set_reset(True)
-    streaming.set_temperature(0)
-    streaming.previous_step[:1].copy_(inputs)
-
-    semantic = semantic.repeat_interleave(4, -1)
-    streaming_inputs = torch.cat([
-        torch.zeros(1, 16, 64),
-        semantic[:, :64][:, None],
-    ], 1)
-    out = streaming(streaming_inputs)[:, :-1]
     streaming_samples = torch.cat([inputs, out.permute(0, 2, 1)], 1).long()
 
     assert torch.allclose(regular_samples, streaming_samples)
